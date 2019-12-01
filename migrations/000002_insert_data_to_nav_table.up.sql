6b6c1a64dcca9911a59f51f6108a4f1aab9cbd35
nikita dudin <rioku.tensey@gmail.com>
2019-12-01 20:53:07 +0200

    refactor migrations

--- migrations/000002_insert_data_to_nav_table.up.sql
+++ migrations/000002_insert_data_to_nav_table.up.sql
@@ -1,5 +1,4 @@
 insert into nav ("id", "order", "title", "uri") values (default, 1, 'Home', '/');
-insert into nav ("id", "order", "title", "uri") values (default, 2, 'Features', '/features');
-insert into nav ("id", "order", "title", "uri") values (default, 3, 'Recipes', '/recipes');
-insert into nav ("id", "order", "title", "uri") values (default, 4, 'Reviews', '/reviews');
-insert into nav ("id", "order", "title", "uri") values (default, 5, 'Contact', '/contact');
+insert into nav ("id", "order", "title", "uri") values (default, 2, 'Recipes', '/recipes');
+insert into nav ("id", "order", "title", "uri") values (default, 3, 'About', '/about');
+insert into nav ("id", "order", "title", "uri") values (default, 4, 'Contact', '/contact');
