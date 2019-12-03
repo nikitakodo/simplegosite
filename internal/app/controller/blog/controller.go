55b076419fe7b206b20a1dc444df95da1b1faa0f
nikita dudin <rioku.tensey@gmail.com>
2019-12-04 00:25:53 +0200

    update controller

--- internal/app/controller/blog/controller.go
+++ internal/app/controller/blog/controller.go
@@ -1,7 +1,10 @@
 package blog
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
 	"github.com/sirupsen/logrus"
 	"math"
 	"net/http"
@@ -160,6 +163,128 @@ func (c *Controller) Recipes(w http.ResponseWriter, r *http.Request) {
 		c.Error(w, r, err)
 		return
 	}
+}
+
+func (c *Controller) About(w http.ResponseWriter, r *http.Request) {
+	tmplName := "blog_pages_about"
+	if res, err := GetCachedView(tmplName, c.Store.Cache); err == nil && len(*res) > 0 {
+		_, err := w.Write([]byte(*res))
+		if err != nil {
+			c.Error(w, r, err)
+			return
+		}
+		return
+	}
+
+	data := map[string]interface{}{}
+	page, err := GetBasicData(c.View, c.Logger, c.Store)
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+	data["page"] = page
+
+	aboutRepo := repository.AboutRepository{Store: c.Store}
+
+	data["about"], err = aboutRepo.FindFirst()
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+
+	//rr := repository.RecipeRepository{Store: c.Store}
+
+	data["facts"] = map[string]interface{}{
+		"Amazing receipies": map[string]interface{}{"icon": "img/icon/1.png", "count": 1850},
+		"Wine pairings":     map[string]interface{}{"icon": "img/icon/2.png", "count": 185},
+		"Meat receipies":    map[string]interface{}{"icon": "img/icon/3.png", "count": 1500},
+		"Desert receipies":  map[string]interface{}{"icon": "img/icon/4.png", "count": 50},
+	}
+
+	content, err := c.View.ProcessTemplate(data, tmplName)
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+	_ = SetCachedView(*content, tmplName, c.Store.Cache)
+	_, err = w.Write([]byte(*content))
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+}
+
+func (c *Controller) Contact(w http.ResponseWriter, r *http.Request) {
+	tmplName := "blog_pages_contact"
+	if res, err := GetCachedView(tmplName, c.Store.Cache); err == nil && len(*res) > 0 {
+		_, err := w.Write([]byte(*res))
+		if err != nil {
+			c.Error(w, r, err)
+			return
+		}
+		return
+	}
+
+	data := map[string]interface{}{}
+	page, err := GetBasicData(c.View, c.Logger, c.Store)
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+	data["page"] = page
+
+	data["info"] = map[string]interface{}{
+		"address": "<p>481 Creekside Lane Avila</p><p>Beach, CA 93424</p>",
+		"phone":   "<p>+53 345 7953 32453</p><p>+53 345 7557 822112</p>",
+		"email":   "<p>yourmail@gmail.com</p>",
+	}
+	data["form_action"] = "/contact_form"
+
+	content, err := c.View.ProcessTemplate(data, tmplName)
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+	_ = SetCachedView(*content, tmplName, c.Store.Cache)
+	_, err = w.Write([]byte(*content))
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+}
+
+func (c *Controller) ContactForm(w http.ResponseWriter, r *http.Request) {
+
+	type Form struct {
+		Name    string
+		Email   string
+		Subject string
+		Message string
+	}
+	form := new(Form)
+	err := r.ParseForm()
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+	decoder := schema.NewDecoder()
+	err = decoder.Decode(form, r.PostForm)
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+	a, err := json.Marshal(form)
+	if err != nil {
+		c.Error(w, r, err)
+		return
+	}
+	_, _ = w.Write(a)
+}
+
+func (c *Controller) NotFound(w http.ResponseWriter, r *http.Request) {
+	c.Logger.Error(r.RequestURI)
+	c.View.Error(w, r, http.StatusNotFound, errors.New(r.RequestURI+" not found"))
+	return
 }
 
 func (c *Controller) Error(w http.ResponseWriter, r *http.Request, err error) {
